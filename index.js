06da6d759ed52002358bffebfa4c23b3a564c04c
George Godwin <georgedaninno@gmail.com>
2023-02-11 18:09:21 +0100

    changes

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app  = express();
 const cors = require("cors");
 
-
+const dotenv = require("dotenv")
+dotenv.config({path: './config.env'})
+console.log(process.env);
      app.use(cors());
 
      app.use(express.json());
